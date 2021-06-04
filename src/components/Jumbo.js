b1100f20499d50309c94bd9701531f184ccb7f0c
emmanuel <emmanuelpallares50@gmail.com>
2021-06-04 15:35:38 -0500

    starter a ecommerce with gatsby starter add styles and basic structure

---
+++ src/components/Jumbo.js
@@ -0,0 +1,13 @@
+import React from "react"
+import { StyledJumbo } from "../styles/components"
+
+export default function Jumbo() {
+  {
+    return (
+      <StyledJumbo>
+        <h2>Texto de prueba para anuncio</h2>
+        <small>Piezas elegantes para la ocasion</small>
+      </StyledJumbo>
+    )
+  }
+}
